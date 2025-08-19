39edaa947cc9466d367e65318afd46dd30259ddb
Lins Thomas <raphealins@gmail.com>
2025-08-19 10:48:20 +0530

    added base path again

--- calendar_project/vite.config.ts
+++ calendar_project/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/calendar_project/", // ✅ use your Netlify subdirectory as base
+  base: "./", // ✅ use your Netlify subdirectory as base
 });
